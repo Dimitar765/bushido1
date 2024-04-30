46b7175b7c00f37cce147cc5853de73e44576722
Dimitar765 <112801140+Dimitar765@users.noreply.github.com>
2024-04-30 10:47:09 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+// import Card from './components/Card'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import Galery from './pages/Galery'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/galery' element={<Galery/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='*' element={<h1>404 Not Found</h1>}/>
+    </Routes>
+    </BrowserRouter>
+    {/* <HomePage /> */}
+    {/* <Card/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,40 @@
+import Card from "../components/Card"
+
+function HomePage() {
+  return (
+    <>
+      <div className="bg-slate-100">
+        <section className="relative">
+          <img className="w-full " src="back.jpg" alt="" />
+          <div className="absolute inset-0 opacity-60"></div>
+          <div className="absolute inset-0 flex items-center justify-center">
+            <h2 className=" text-7xl font-bold text-red-500 uppercase">Karate Klub Bushido</h2>
+          </div>
+        </section>
+        <div className="flex justify-between">
+          <div className="mt-36">
+            <img className="" src="wado.jpg" alt="" />
+          </div>
+          <div className="mt-20 text-justify text-2xl p-5 w-2/3 tracking-tighter">
+            <p> Karate klub Bushido SAN osnovan je 10.marta 2020. godine i jedan je od retkih klubova u kojem se uci Wado Kai/Wado Ryu stil.
+              Osnivaci su Aleksandar Makic, nosilac crnog pojasa 3.dan, licencirani trener i sudija. Uros Jovanovic, nosilac crnog pojasa 1.dan, licencirani trener i sudija. Marko Makic, nosilac crnog pojasa 1.dan i licencirani trener.
+              Lokacija na kojoj drzimo treninge je u okviru teretane Puls Fitnes na Novom Beogradu(Bulevar Arsenija Carnojevica 165).
+              Wado-Ryu ili Wadoryu stil je jedan od najvecih 6 stilova u karateu, pored Shotokana, Shitoryu, Gojoryu, Kyokushin  i Kenpo.Osnivac je Hironori Otsuka.Vizuelno, jako je slican SHotokan stilu, ali ima pojedinih kljucnihb razlika u samim tehnikama i nacinu njihovog izvodjenja.
+              Zasto Wadoryu? Zato sto u sebi sadrzi tradicionalni Shotokan duh sa implementovanim elementima Judoa i Jiu Jitse.Sta to u praksi znaci?Visi stavovi, zdravija kolena, efektivniji nacini nosenja sa modernim borilackim sportovima.
+              Karate klub Bushido SAN clan je Srpske Karate Unije u okviru WKC-a(World Karate Confederation);I pored toga, nas klub ucestvuje na takmicenjima i drugih federacija i unija i tako odrzavamo zdrav odnos sa svim ljubiteljima karate vestine u regionu.
+              Sustinski cilj osnivanja bila je iskrena zelja da nasoj deci, omladini ali i starijima omogucimo jednu zdravu sredinu u kojoj ce vredno trenirati, ici zajedno na takmicenja, druziti se ali i danoinocno usavrsavati se.
+              Od pocetka rada kluba, imamo nosioce zlatnih, srebrnih i bronzanih medalja sa kako balkanskih i evropskih, tako i svetskih takmicenja.</p>
+          </div>
+          <div className="mt-36">
+            <img src="wado.jpg" alt="" />
+          </div>
+        </div>
+
+        <Card />
+
+      </div>
+    </>
+  )
+}
+
+export default HomePage
