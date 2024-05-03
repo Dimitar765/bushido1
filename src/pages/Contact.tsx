93324f48dc0a17eb29e85125cb41462a8d7982c1
Dimitar765 <112801140+Dimitar765@users.noreply.github.com>
2024-05-03 19:10:09 +0200

    minor changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -17,7 +17,7 @@ const Contact: React.FC = () => {
             <a href="https://www.instagram.com/_bushido_san_/" target="_blank" rel="noopener noreferrer">{<FaInstagramSquare />}</a>
           </div>
         </div>
-        <div>
+        <div className="animate-fade">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2830.107596911876!2d20.398502087684463!3d44.81937259647881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a657f39740533%3A0x181daa1cd60e774c!2sBulevar%20Arsenija%20%C4%8Carnojevi%C4%87a%20165%2C%20Beograd%2011070%2C%20Serbia!5e0!3m2!1sen!2smk!4v1714646472126!5m2!1sen!2smk" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
--- src/pages/Galery.tsx
+++ src/pages/Galery.tsx
@@ -4,42 +4,42 @@ function Gallery() {
     <>
 
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4 bg-slate-200">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 bg-slate-200">
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="1.jpeg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="4.jpeg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="2.jpeg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="5.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="3.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="6.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="4.jpeg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="1.jpeg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="5.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="2.jpeg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="6.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="3.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="7.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="7.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="8.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="8.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="9.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="9.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="10.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="10.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="11.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="11.jpg" alt="" />
         </div>
         <div>
-          <img className="h-auto max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="12.jpg" alt="" />
+          <img className="h-full max-w-full rounded-lg animate-fade animate-delay-[100ms]" src="12.jpg" alt="" />
         </div>
       </div>
 
